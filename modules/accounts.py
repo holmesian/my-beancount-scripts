e11d1bc64dfd2704e5664127df0e306a703073c5
Holmesian <Home@holmesian.org>
2022-01-02 13:38:53 +0800

    updata for myselft

--- modules/accounts.py
+++ modules/accounts.py
@@ -28,14 +28,20 @@ public_accounts = [
 ]
 
 credit_cards = {
-    '中信银行': 'Liabilities:CreditCard:CITIC',
+    '招商银行5993': 'Liabilities:CreditCard:招商银行5993',
+    '建设银行7816': 'Liabilities:CreditCard:建设银行7816',
+    '平安银行0912': 'Liabilities:CreditCard:平安银行0912',
+    '花呗': 'Liabilities:Company:Huabei',
+
 }
 
 accounts = {
     "余额宝": 'Assets:Company:Alipay:MonetaryFund',
     '余利宝': 'Assets:Bank:MyBank',
-    '花呗': 'Liabilities:Company:Huabei',
-    '建设银行': 'Liabilities:CreditCard:CCB',
+    '交通银行8447': 'Assets:DebitCard:交通银行8447',
+    '招商银行9061': 'Assets:DebitCard:招商银行9061',
+    '建设银行2057': 'Assets:DebitCard:建设银行2057',
+    '平安银行': 'Assets:DebitCard:平安银行1940',
     '零钱': 'Assets:Balances:WeChat',
 }
 
@@ -46,15 +52,16 @@ descriptions = {
     '花呗收钱服务费': 'Expenses:Fee',
     '自动还款-花呗.*账单': 'Liabilities:Company:Huabei',
     '信用卡自动还款|信用卡还款': get_credit_return,
-    '外卖订单': get_eating_account,
-    '美团订单': get_eating_account,
-    '上海交通卡发行及充值': 'Expenses:Transport:Card',
-    '地铁出行': 'Expenses:Transport:City',
-    '火车票': 'Expenses:Travel:Transport',
+    '汽车加油': 'Liabilities:Company:中石化',
+    # '外卖订单': get_eating_account,
+    # '美团订单': get_eating_account,
+    # '上海交通卡发行及充值': 'Expenses:Transport:Card',
+    # '地铁出行': 'Expenses:Transport:City',
+    # '火车票': 'Expenses:Travel:Transport',
 }
 
 anothers = {
-    '上海拉扎斯': get_eating_account
+    # '上海拉扎斯': get_eating_account
 }
 
 incomes = {
